a46886672fc4b27a60af9585b652dca15ed9af0e
CircleKEgypt <circlekegyptit@gmail.com>
2024-02-15 14:51:52 +0200

    t

--- CK/obj/Debug/net8.0/CK.AssemblyInfo.cs
+++ CK/obj/Debug/net8.0/CK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e26bf8d93c6b21b8c84a10bdb303a12ee32fe9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c197b9c725f2f453b42fdd354272d1efb51714d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
